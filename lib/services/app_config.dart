3c62b30137b79f1fadf531a7f9ddd8e5614ddbff
Bruno A. Morari <bruno.morari@webmotors.com.br>
2026-09-22 15:17:05 -0300

    fix secret key

---
+++ lib/services/app_config.dart
@@ -0,0 +1,3 @@
+class AppConfig {
+  static const String apiKey = String.fromEnvironment('API_KEY');
+}
--- lib/services/current_weather.dart
+++ lib/services/current_weather.dart
@@ -2,9 +2,13 @@ import 'package:http/http.dart' as http;
 
 import 'dart:convert';
 
+import 'package:weather_monitor/services/app_config.dart';
+
 Future<Map<String, dynamic>> getCurrentWeather(String city) async {
+  final String apiKey = AppConfig.apiKey;
+
   final url = Uri.https('api.weatherapi.com', '/v1/current.json', {
-    'key': 'e5c0cd66c8f8445199d170307261809',
+    'key': apiKey,
     'q': city,
     'lang': 'pt',
   });
